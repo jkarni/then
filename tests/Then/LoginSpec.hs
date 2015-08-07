eb044a9e617055f3fa2580f133bd24440915d042
Julian K. Arni <jkarni@gmail.com>
2015-08-07 16:32:40 +0200

    Passing incorrect pwd tests

--- tests/Then/LoginSpec.hs
+++ tests/Then/LoginSpec.hs
@@ -1,12 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Then.LoginSpec (spec) where
 
-import           Control.Monad
 import           Data.Aeson                 (decode)
-import qualified Data.ByteString.Char8      as BS
 import           Data.Maybe                 (fromJust)
-import           Data.Text                  ()
-import           Database.PostgreSQL.Simple
+import qualified Data.Text                  as Text
 import           Servant
 import           Test.Hspec
 import           Test.QuickCheck            (property, (==>))
@@ -42,19 +40,19 @@ loginByUsernameSpec = beforeAll setupDB $ describe "loginByUsername" $ do
   context "the user exists, but password is incorrect" $ do
 
     it "returns a 400" $ \conn -> property $ \name pwd pwd' email ->
-      pwd /= pwd' ==> do
+      pwd /= pwd' && Text.length name <= 80 ==> do
         let u = LoginByUsername name pwd
         withUser conn (User name pwd' email) $ loginByUsername conn u `shouldLeftSatisfy`
           ((== 400) . errHTTPCode)
 
     it "returns an error description" $ \conn -> property $ \name pwd pwd' email -> do
-      pwd /= pwd' ==> do
+      pwd /= pwd' && Text.length name <= 80 ==> do
         let u = LoginByUsername name pwd
-        withUser conn (User name pwd email) $ loginByUsername conn u `shouldLeftSatisfy`
+        withUser conn (User name pwd' email) $ loginByUsername conn u `shouldLeftSatisfy`
           (\y -> (== [loginError]) . errors . fromJust . decode $ errBody y)
 
     it "has error status field" $ \conn -> property $ \name pwd pwd' email -> do
-      pwd /= pwd' ==> do
+      pwd /= pwd' && Text.length name <= 80 ==> do
         let u = LoginByUsername name pwd
-        withUser conn (User name pwd email) $ loginByUsername conn u `shouldLeftSatisfy`
+        withUser conn (User name pwd' email) $ loginByUsername conn u `shouldLeftSatisfy`
           (\y -> (== Failure) . status . fromJust . decode $ errBody y)
